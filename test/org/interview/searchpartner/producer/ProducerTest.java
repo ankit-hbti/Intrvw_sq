f6a12959d5f0517586fc5aaae090cb217f98c96f
Ankit Agarwal <ankit.hbti@gmail.com>
2018-07-01 18:40:39 +0530

    Commitiing the code

---
+++ test/org/interview/searchpartner/producer/ProducerTest.java
@@ -0,0 +1,45 @@
+package org.interview.searchpartner.producer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.interview.searchpartner.bean.MachineParts;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProducerTest {
+	
+	private int noOfMachines = 1;
+	private int noOfBolts = 2;;
+	private BlockingQueue<MachineParts> queue=null;
+	private Producer producer;
+
+
+
+	@Before
+	public void setUp() throws Exception {
+		queue = new ArrayBlockingQueue<MachineParts>(noOfMachines + noOfBolts);
+		producer = new Producer("TestProducer", noOfMachines, noOfBolts, queue);
+	}
+
+
+
+	@Test
+	public void testRun() {
+		producer.run();
+		assertEquals(false, queue.isEmpty());
+		assertEquals(3, queue.size());
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("TestProducer",producer.toString());
+	}
+
+}
