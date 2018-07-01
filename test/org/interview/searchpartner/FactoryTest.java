e111c1a763d326ded4b448f7d4bd3b0ad2b8c53d
Ankit Agarwal <ankit.hbti@gmail.com>
2018-07-01 19:16:34 +0530

    Commitiing the code

---
+++ test/org/interview/searchpartner/FactoryTest.java
@@ -0,0 +1,31 @@
+package org.interview.searchpartner;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+public class FactoryTest {
+	
+	private static AtomicInteger totalProducts = new AtomicInteger(0);
+
+	@Test
+	public void testMain() {
+		Factory.main(new String []{"3", "6", "5"});
+		assertEquals(3, Factory.getTotalProducts().get());
+	}
+
+	@Test
+	public void testGetTotalProducts() {
+		Factory.setTotalProducts(totalProducts);
+		assertEquals(0, Factory.getTotalProducts().get());
+	}
+
+	@Test
+	public void testSetTotalProducts() {
+		Factory.setTotalProducts(totalProducts);
+		assertEquals(0, Factory.getTotalProducts().get());
+	}
+
+}
